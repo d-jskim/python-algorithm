7779f66900e935d792b278c04cabd1e84d15c94b
d-jskim <57737000+d-jskim@users.noreply.github.com>
2026-08-12 17:40:16 +0900

    [프로그래머스] L0 - 나이 출력 (#3)

---
+++ programmers/L0_기초입문/입문/003_나이_출력.py
@@ -0,0 +1,11 @@
+# 나이 출력
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120820
+# 알고리즘: 기초
+# 작성자: 학생
+# 작성일: 2026. 08. 12. 17:40:15
+
+def solution(age):
+    answer = 0
+    answer = 2022 - age + 1
+    return answer
